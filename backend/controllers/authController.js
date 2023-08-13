1266cc651b10a9abd5e886b3d25b44200c34ffee
Bernice <bernicetoh2003@gmail.com>
2023-08-13 20:02:22 +0800

    create login endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,11 +2,28 @@ const catchAsync = require("../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
-const signToken = (id) => {
+const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+  res.status(statusCode).json({
+    status: "success",
+    token: token,
+  });
 };
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -21,4 +38,19 @@ exports.signup = catchAsync(async (req, res, next) => {
     token,
     data: { user: newUser },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password", 400));
+  }
+  // 2) check if user exists && pw is correct
+  // select(+password) puts the field back in the output
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+  createSendToken(user, 200, res);
 });
