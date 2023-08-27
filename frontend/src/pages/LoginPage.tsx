01b17f93ae3aa8a2d1788af92213718c809366db
Bernice <bernicetoh2003@gmail.com>
2023-08-27 16:11:25 +0800

    Add dynamic logo to Login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,7 +8,7 @@ import beanBrown from "../assets/bean-brown.svg";
 import AuthContext from "../context/AuthProvider";
 import { UserDetails } from "../models/user.model";
 import Cookies from "js-cookie";
-import { motion } from "framer-motion";
+import { easeInOut, motion, useMotionValue, useTransform } from "framer-motion";
 function LoginPage() {
   const navigate = useNavigate();
   const { setAuthState, authState } = useContext(AuthContext);
@@ -94,27 +94,91 @@ function LoginPage() {
     setEmail("");
     setPassword("");
   }
+  const x = useMotionValue(200);
+  const y = useMotionValue(200);
+
+  const rotateXBrown = useTransform(y, [0, 400], [20, -20]);
+  const rotateYBrown = useTransform(x, [0, 400], [-20, 20]);
+  const rotateXWhite = useTransform(y, [0, 400], [45, -45]);
+  const rotateYWhite = useTransform(x, [0, 400], [-45, 45]);
+
+  function handleMouse(event: any) {
+    // console.log(event);
+    const rect = event.currentTarget.getBoundingClientRect();
+
+    x.set(event.clientX - rect.left);
+    y.set(event.clientY - rect.top);
+  }
+
+  const boxRef1 = useRef<HTMLDivElement>(null);
+
+  const isMouseInBound = (e: any) => {
+    if (!boxRef1 || !boxRef1.current) return;
+    const eleBounds = boxRef1.current.getBoundingClientRect();
+    let ret = false;
+    if (e.clientX >= eleBounds.left && e.clientX <= eleBounds.right) {
+      ret = true;
+    } else {
+      ret = false;
+    }
+  };
+
+  const resetPosition = (e: any) => {
+    x.set(100);
+    y.set(100);
+
+    // e.currentTarget.transition = {""}
+  };
 
   return (
     <div className={styles["loginscreen"]}>
       <div className={styles["login-content"]}>
         <div className={styles["login-card"]}>
           <div className={styles["left-container"]}>
-            <img src={beanWhite} alt="bean white" id="bean-logo" />
-            <div
-              ref={brownBean}
+            <motion.div
               style={{
-                top: `${mousePosition.y}px`,
-                left: `${mousePosition.x}px`,
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                placeItems: "center",
+                placeContent: "center",
+                borderRadius: 30,
+                backgroundColor: "rgba(255, 255, 255, 0.05)",
+                perspective: 400,
                 position: "relative",
               }}
+              onMouseEnter={isMouseInBound}
+              onMouseLeave={resetPosition}
+              onMouseMove={handleMouse}
+              ref={boxRef1}
             >
-              {/* <img src={beanBrown} alt="bean white" id="bean-logo" /> */}
-            </div>
-
-            <div style={{ color: "black" }}>
-              {JSON.stringify(mousePosition)}
-            </div>
+              <motion.img
+                src={beanBrown}
+                className={styles["moving-img"]}
+                // transition={{ ease: "linear", duration: 2 }}
+                style={{
+                  zIndex: 1,
+                  width: 150,
+                  height: 150,
+                  rotateX: rotateXBrown,
+                  rotateY: rotateYBrown,
+                  position: "absolute",
+                }}
+              />
+              <motion.img
+                src={beanWhite}
+                className={styles["moving-img"]}
+                style={{
+                  zIndex: 0,
+                  width: 160,
+                  height: 160,
+                  left: 110,
+                  rotateX: rotateXWhite,
+                  rotateY: rotateYWhite,
+                  position: "absolute",
+                }}
+              />
+            </motion.div>
           </div>
 
           <div className={styles["right-container"]}>
@@ -123,10 +187,9 @@ function LoginPage() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.3 }}
-              style={{ height: "100%", width: "100%" }}
+              style={{ width: "100%" }}
             >
               <div className={styles["header"]}>
-                <img src={logo} alt="logo" className={styles["logo"]} />
                 <h1>Welcome back!</h1>
                 <p>Please enter your details</p>
               </div>
