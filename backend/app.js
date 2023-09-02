022844e0f98dea4d15c65e4d47129e677c9af1e1
Bernice <bernicetoh2003@gmail.com>
2023-09-02 12:59:29 +0800

    add mongoose

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,6 @@ const app = express();
 // Global middlewares
 app.use(cors());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, "")));
 
 // Set security HTTP headers
 app.use(helmet());
