b42d043adfda7d4b81e5c2d5db1525fabf87fa1a
Bernice <bernicetoh2003@gmail.com>
2023-09-02 13:37:07 +0800

    fix bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,8 @@
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const User = require("../models/userModel");
-const AppError = require("../utils/AppError");
+const AppError = require("./../utils/appError");
 const crypto = require("crypto");
 
 const signToken = (id) =>
